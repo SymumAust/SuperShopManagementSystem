4457f7f4362f7795d99433b31cf21215f1c2bc02
Md. Symum Hossain <190204105@aust.edu>
2023-02-04 05:04:13 +0600

    Home Page First Part
    Slider, Navbar, Heading, etc

---
+++ SupermanShop/SupermanShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupermanShop.Startup))]
+namespace SupermanShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
