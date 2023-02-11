d5368ee721807349937c2f71e3d0e8cf63600c7a
101safin <91382618+101safin@users.noreply.github.com>
2023-02-12 00:12:49 +0600

    part7

--- SupermanShop/SupermanShop/Controllers/AdminController.cs
+++ SupermanShop/SupermanShop/Controllers/AdminController.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SupermanShop.Models;
+using Newtonsoft.Json;
 
 namespace SupermanShop.Controllers
 {
@@ -24,6 +25,26 @@ namespace SupermanShop.Controllers
         {
             List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
             return View(allcategories);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd);
+
         }
     }
 }
